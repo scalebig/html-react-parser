010d7ead6815427a16b037b49d868d2a7dcadd0b
Timothy Smith <>
2018-07-30 14:35:24 -0500

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,20 @@ module.exports = {
       amd: 'react'
     }
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
